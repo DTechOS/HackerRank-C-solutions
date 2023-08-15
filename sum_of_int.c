b3c23eb467860eb902da0d06f99caa8ee73773a3
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-08-15 18:09:57 -0500

    Create sum_of_int.c

---
+++ sum_of_int.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+   
+   int digit;  
+   int counter = 0;  
+
+while(n >0) {
+    counter += n % 10;
+    n /= 10;
+}
+printf("%d",counter);
+
+    
+    //Complete the code to calculate the sum of the five digits on n.
+    return 0;
+}
