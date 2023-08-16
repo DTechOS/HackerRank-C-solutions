2c84dca6e360464f9b894c2b60d9aa25c1a3a31b
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-08-16 09:20:17 -0500

    Update max_of_ints.c

--- max_of_ints.c
+++ max_of_ints.c
@@ -21,3 +21,31 @@ int main() {
 /* 
 Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them. 
 */
+
+/* 
+max_of_four:
+        push    rbp
+        mov     rbp, rsp
+        mov     DWORD PTR [rbp-4], edi
+        mov     DWORD PTR [rbp-8], esi
+        mov     DWORD PTR [rbp-12], edx
+        mov     DWORD PTR [rbp-16], ecx
+        mov     edx, DWORD PTR [rbp-4]
+        mov     eax, DWORD PTR [rbp-8]
+        cmp     edx, eax
+        cmovge  eax, edx
+        mov     DWORD PTR [rbp-4], eax
+        mov     edx, DWORD PTR [rbp-4]
+        mov     eax, DWORD PTR [rbp-12]
+        cmp     edx, eax
+        cmovge  eax, edx
+        mov     DWORD PTR [rbp-4], eax
+        mov     edx, DWORD PTR [rbp-4]
+        mov     eax, DWORD PTR [rbp-16]
+        cmp     edx, eax
+        cmovge  eax, edx
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-4]
+        pop     rbp
+        ret
+*/
