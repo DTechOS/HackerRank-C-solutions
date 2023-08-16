3338fc1d13665c580f526ed5b09f7cd8b399d3dc
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-08-15 20:01:46 -0500

    Create conditionals.c

---
+++ conditionals.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+int main()
+{
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+    char char_list[9][10] = {
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
+
+if (n <= 9) 
+{
+  printf("%s", char_list[n - 1]);
+} else {
+    printf("Greater than 9");
+    }
+    return 0;
+} 
