e3b38da73d9b71080929959df4e9f599f5db4a76
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-08-18 05:40:02 -0500

    Create boxes_pass_reject.c

---
+++ boxes_pass_reject.c
@@ -0,0 +1,25 @@
+struct box
+{
+	/**
+	* Define three fields of type int: length, width and height
+	*/
+    int length,width,height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+    return b.length*b.width*b.height;
+}
+
+int is_lower_than_max_height(box b) {
+    if (b.height < MAX_HEIGHT) 
+    {
+      return 1;
+    } else {
+    return 0;
+    }
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+}
