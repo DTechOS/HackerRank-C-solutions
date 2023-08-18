22666da8cef6417cf6221250efe2e2059253e701
DTechOS <62873327+DTechOS@users.noreply.github.com>
2023-08-18 04:52:03 -0500

    Update reverse_an_array.c

--- reverse_an_array.c
+++ reverse_an_array.c
@@ -9,10 +9,15 @@ int main()
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
-    // iterate over half of elements
-     for(i=0; i<num/2; i++)
-    {
-        temp = *(arr+i); // initalize temp arr
-        *(arr+i) = *(arr+num-1-i); // swap elements in reversed order
-        *(arr+num-1-i) = temp; // assign reversed array to temp
-    }
+    
+    for(int i = num-1; i>=0; i--){
+        temp = arr[i];
+        printf("%d ",temp);
+       }
+
+    /* Write the logic to reverse the array. */
+
+    for(i = 0; i < num; i++)
+       // printf("%d ", *(arr + i));
+    return 0;
+}
